2458c0c2e2f0adc19a151d55cdb8fb0614ae9b92
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2025-03-31 21:08:52 -0500

    add app version service

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { SeedService } from './seed.service';
 
-@Controller('seed')
+@Controller()
 export class SeedController {
     constructor(private readonly seedService: SeedService) { }
 
-    @Post()
+    @Post('seed')
     async seed() {
         return await this.seedService.seed();
+    }
+
+    @Get('app-version')
+    getMinimumVersion() {
+        return { version: '1.0.0' };
     }
 }
